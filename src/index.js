f26d62ea71ed2f564603278a1b6e9135326f0874
ikkromm18 <muhammadikkrom749@gmail.com>
2023-12-26 16:40:04 +0700

    menampilkan hal yang harus dilakukan(do) dengan mencetak object todos, tampilan masih berantakan

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ import generateToDoObject from './scripts/generateToDoObject';
 const todos = [];
 const RENDER_EVENT = 'render-todo';
 
+//membuat objek
 function addTodo(){
+
+  //mengambil nilai dari form
   const toDo = document.getElementById('to-do').value;
   const deadline = document.getElementById('deadline').value;
 
+  //mengenerate id
   const generateID = generateId();
 
   const toDoObject = generateToDoObject(generateID, toDo, deadline, false);
@@ -18,9 +22,30 @@ function addTodo(){
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
+//  membuat todo
+function makeToDo(toDoObject) {
+  // membuat elemen h3 dan mengambil nilai dari objecl toDo
+  const toDoTitle = document.createElement('h3') ;
+  toDoTitle.innerText = toDoObject.toDo;
+  
+  // membuat elemen h3 dan mengambil nilai dari objecl deadline
+  const deadlineToDo = document.createElement('p')
+  deadlineToDo.innerText = toDoObject.deadline;
  
+  //appenchild nilai todotitle dan deadlinetodo
+  const containerDo = document.querySelector('#do');
+  containerDo.appendChild(toDoTitle);
+  containerDo.appendChild(deadlineToDo);
+ }
+
 document.addEventListener(RENDER_EVENT, function () {
-  console.log(todos);
+  //mengosongkan nilai #do sebelum menambahkan do baru
+  const containerDo = document.querySelector('#do');
+  containerDo.innerHTML='';
+
+  for (const todoItem of todos) {
+    const todoElement = makeToDo(todoItem);
+  }
 });
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -30,3 +55,6 @@ document.addEventListener('DOMContentLoaded', function(){
     addTodo();
   })  
 });
+
+
+
